42a39a5f3c7fdfdf2a92359b5aad25d55e3333a6
Honza Brázdil <jbrazdil@redhat.com>
2018-08-28 14:48:36 +0200

    Add file for parsing config files

---
+++ crt/config_parser.py
@@ -0,0 +1,23 @@
+#!/bin/python2.7
+
+import sys
+import json
+
+def parse_pnc_cli_conf(file):
+    with open(file, 'r') as f:
+        config = json.load(f)
+        with open("pnc-cli.conf", "w") as of:
+            of.write("[PNC]\n")
+            of.write("pncurl = " + config['pncUiAddress'].replace("/pnc-web","") + "\n")
+            of.write("keycloakUrl = " + config['keycloakAddress'] + "\n")
+            of.write("keycloakrealm = pncredhat\n")
+            of.write("keycloakclientid = pncdirect\n")
+            of.write("username = " + config['pncUser'] + "\n")
+            of.write("password = " + config['pncPassword'] + "\n")
+
+def main():
+    # print command line arguments
+    parse_pnc_cli_conf(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
